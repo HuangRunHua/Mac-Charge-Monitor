eac28cc80494547eb41d575b71328781ec118269
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-01-09 11:49:57 +0800

    Initial Commit

---
+++ Mac Charge Monitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mac Charge Monitor
+//
+//  Created by Runhua Huang on 2022/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mac Charge Monitor/Mac_Charge_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_Charge_MonitorApp.swift
+//  Mac Charge Monitor
+//
+//  Created by Runhua Huang on 2022/1/9.
+//
+
+import SwiftUI
+
+@main
+struct Mac_Charge_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
