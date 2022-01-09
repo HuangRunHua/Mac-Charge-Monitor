3bb3bf3cc4539cf2774b049783551f72d623fff9
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-01-09 21:09:56 +0800

    添加App图标

--- Mac Charge Monitor/Menu/MacAnimationView.swift
+++ Mac Charge Monitor/Menu/MacAnimationView.swift
@@ -11,19 +11,27 @@ import Cocoa
 
 struct MacAnimationView: View {
     
-    let queuePlayer = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "macbookair", ofType: "mov")!))
+    @State var queuePlayer = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "macbookair", ofType: "mov")!))
+    
+    @EnvironmentObject var deviceStatus: DeviceStatus
     
     var body: some View {
         AVPlayerControllerRepresented(player: queuePlayer)
             .onAppear {
                 queuePlayer.play()
             }
+            .onChange(of: deviceStatus.deviceInfo.isCharging) {newValue in
+                if newValue {
+                    queuePlayer.seek(to: CMTime.zero)
+                    queuePlayer.play()
+                }
+            }
     }
 }
 
 struct MacAnimationView_Previews: PreviewProvider {
     static var previews: some View {
-        MacAnimationView()
+        MacAnimationView().environmentObject(DeviceStatus())
     }
 }
 
